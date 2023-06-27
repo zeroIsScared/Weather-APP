1b9b6abca3cd259bd8a23fb9ffb2c07cc077076d
Natalia <natalia.levinta@amdaris.com>
2023-06-27 10:41:59 +0300

    Added cache logic

--- app.js
+++ app.js
@@ -7,12 +7,12 @@
 // 6. try to make use of "deep destructuring" while you "extract" data from the response
 // 7. don't forget to display a corresponding message in the case the server didn't give the weather data
 
-const form= document.querySelector('#form');
+const form = document.querySelector('#form');
 const input = document.querySelector('input');
 const div = document.querySelector('#output');
 
-const saveDataToCache = (data) => {
-   localStorage.setItem('data', JSON.stringify(data));
+const saveDataToCache = (key, data) => {
+    localStorage.setItem(key, JSON.stringify(data));
 }
 
 const loadDataFromCache = () => {
@@ -20,63 +20,88 @@ const loadDataFromCache = () => {
 }
 
 
-const render= (data) =>{
-    //console.log(data);    
-    const{main: {temp}, name: cityName } = data;
+const checkDataCache = (key) => {
+    return localStorage.getItem(key);
 
-   // console.log(temp);
-    div.innerHTML= `<p>The temperature in ${cityName} at the moment is <strong>${Math.round(temp)} C</strong><p>`;
 }
 
-function submitForm (cb) {           
-   
-    const city = input.value;    
+const load = (cb) => {
+    //1. check the cache
+    if (checkDataCache('data')) {
+        // 2. take from cache
+
+        data = loadDataFromCache('data');
+        console.log(data);
+        cb(data);
+
+    } else {
+        // 3. take from API
+
+        loadDataFromAPI((data) => {
+            console.log(data);
+            saveDataToCache("data", data);
+            cb(data);
+        })
+    }
+}
+
+const render = (data) => {
+    console.log(data);
+    const { main: { temp }, name: cityName } = data;
+
+    console.log(temp);
+    div.innerHTML = `<p>The temperature in ${cityName} at the moment is <strong>${Math.round(temp)} C</strong><p>`;
+}
+
+function loadDataFromAPI(cb) {
+
+    const city = input.value;
     let cityName;
 
-    if( city !== "" || city !== " "){
-    cityName =  city.trim();  
+    if (city !== "" || city !== " ") {
+        cityName = city.trim();
     }
 
     let xhr = new XMLHttpRequest();
     const apiKey = `3cf84b8c25e74a840e93ed93926eb80f`;
-   
+
     xhr.open(
-     'GET',
-     `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`);
-    
+        'GET',
+        `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`);
+
     xhr.send();
     xhr.onload = () => {
         let data = JSON.parse(xhr.responseText);
-        //console.log(data);
-        if (data !== []) { 
-            const [{lat, lon}] =data;
-            
+        console.log(data);
+        if (data !== []) {
+            const [{ lat, lon }] = data;
+
             console.log(lon, lat)
             xhr.open(
                 'GET',
                 `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
             )
             xhr.send();
-            xhr.onload = () =>{
+            xhr.onload = () => {
                 let data = JSON.parse(xhr.responseText);
                 cb(data);
-    
+
             }
         } else {
-            div.textContent= `Sorry there were no results found for your city!`
+            div.textContent = `Sorry there were no results found for your city!`
         }
-    }  
-}   
-  
+    }
+}
 
-form.addEventListener('submit', (e)=> {
-        e.preventDefault();       
-     });
 
-form.addEventListener('submit', ()=>{
-    submitForm(render);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
 });
-  
-        
-        
-   
+
+form.addEventListener('submit', () => {
+    load(render);
+});
+
+
+
+
